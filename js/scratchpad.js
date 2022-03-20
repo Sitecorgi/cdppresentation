3d51b733f890b9115cb3d1649877dfba2270af9f
JDarrall <james@sitecorgi.net>
2022-03-20 15:56:32 +0000

    connection

--- js/scratchpad.js
+++ js/scratchpad.js
@@ -7,21 +7,30 @@
     output.email = guest.email;
   }
 
+  const addEventDataToOutput = function (events) {
+    events.forEach(function (event) {
+      if (
+        event.type === "ADD" &&
+        event.arbitraryData &&
+        event.arbitraryData.product &&
+        event.arbitraryData.product.price
+      ) {
+        output.totalPrice =
+          event.arbitraryData.product.price *
+          event.arbitraryData.product.quantity;
+        output.spaBreak = event.arbitraryData.product.name;
+        output.sku = event.arbitraryData.product.referenceId;
+        output.orderedAt = event.arbitraryData.product.orderedAt;
+        output.link = "https://budapestspabreakscdpdemo.website/";
+      }
+    });
+    output.sessionAbandonedAt = guest.sessions[i].endedAt;
+  };
+
   if (guest.sessions && guest.sessions.length) {
     for (var i = 0; i < guest.sessions.length; i++) {
       if (guest.sessions[i].cartType === "ABANDONED") {
-        guest.sessions[i].events.forEach(function (event) {
-          if (
-            event.type === "ADD" &&
-            event.arbitraryData &&
-            event.arbitraryData.product &&
-            event.arbitraryData.product.price
-          ) {
-            output.totalPrice += event.arbitraryData.product.price;
-            output.spaBreak = event.arbitraryData.product.name;
-          }
-        });
-        output.sessionAbandonedAt = guest.sessions[i].endedAt;
+        addEventDataToOutput(guest.sessions[i].events);
         break;
       }
     }
